76f3fcc0c5b9ab15c3c76332f3017c2d5dcb897c
aawol <119796909+aawol@users.noreply.github.com>
2022-12-06 16:05:03 +0300

    Update and rename ph_levels.py to grade_levels.py
    # grade variabel
    grade=int(input('Enter the grade value (0-100):' ))
    if grade > 90:
      print('A')
    elif grade > 80:
      print('B')
    elif grade > 70:
      print('C')
    elif grade > 60:
      print('D')
    else:
      print('F')

---
+++ 3-control-flow/grade_levels.py
@@ -0,0 +1,12 @@
+# grade variabel
+grade=int(input('Enter the grade value (0-100):' ))
+if grade > 90:
+  print('A')
+elif grade > 80:
+  print('B')
+elif grade > 70:
+  print('C')
+elif grade > 60:
+  print('D')
+else:
+  print('F')
